a05c74e0d09938dad3b2413597444acb7ccff90b
MaxChip <1Max0Lag1@gmail.com>
2025-01-15 21:31:36 -0500

    updated some flags

--- src/main.c
+++ src/main.c
@@ -8,8 +8,7 @@
 const char* MAKEUP_VERSION = "0.0.1";
 
 /* Flags */
-const char* MAKEUP_CREATE_FLAG = "start";
-const char* MAKEUP_BUILD_FLAG = "build";
+const char* MAKEUP_CREATE_FLAG = "create";
 const char* MAKEUP_VERSION_FLAG_SHORT = "-v";
 const char* MAKEUP_VERSION_FLAG = "--version";
 const char* MAKEUP_COMMANDS_FLAG_SHORT = "-h";
@@ -31,15 +30,22 @@ int main(int argc, char* argv[])
         exit(1);
     }
 
-    if(argc >= 1)
+    // check for flags
+    if(argc > 1)
     {
-        if(argv[1] == MAKEUP_COMMANDS_FLAG_SHORT || argv[1] == MAKEUP_COMMANDS_FLAG)
+        if(strcmp(argv[1], MAKEUP_COMMANDS_FLAG_SHORT) == 0 || strcmp(argv[1], MAKEUP_COMMANDS_FLAG) == 0)
         {
-
+            exit(0);
         }
-        else if(argv[1] == MAKEUP_VERSION || argv[1] == MAKEUP_VERSION_FLAG_SHORT)
+        else if(strcmp(argv[1], MAKEUP_VERSION_FLAG) == 0 || strcmp(argv[1], MAKEUP_VERSION_FLAG_SHORT) == 0)
         {
             printf("Makeup version %s\n", MAKEUP_VERSION);
+            exit(0);
+        }
+        else if(strcmp(argv[1], MAKEUP_CREATE_FLAG) == 0 || strcmp(argv[1], MAKEUP_VERSION_FLAG_SHORT) == 0)
+        {
+            printf("Created template Makeup file at '%s'\n", cwd);
+            exit(0);
         }
     }
     // check for Makeup file
@@ -47,7 +53,7 @@ int main(int argc, char* argv[])
     snprintf(full_cwd, sizeof(full_cwd), "%s%s", cwd, "/Makeup");
     if(access(full_cwd, F_OK) != 0)
     {
-        printf("'Makeup' file does not exist in '%s', please make a Makeup file\n", cwd);
+        printf("Makeup file does not exist in '%s', please make a Makeup file\n", cwd);
         exit(1);
     }
 
